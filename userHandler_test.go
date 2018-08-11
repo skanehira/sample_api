95e252491ef67d9e251cd062a6d8d66c606db8b0
skanehira <sho19921005@gmail.com>
2018-08-11 21:52:24 +0900

    add routing function

---
+++ userHandler_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestUserRegister(t *testing.T) {
+
+}
+
+func TestUserUpdator(t *testing.T) {
+
+}
+
+func TestGetUser(t *testing.T) {
+
+}
+
+func TestUserDeleter(t *testing.T) {
+
+}
